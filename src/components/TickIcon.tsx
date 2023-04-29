74a0699b64264fec03c70f9a05f9497e3d52f339
richfjiq <rfjiq1986@gmail.com>
2023-04-28 21:37:04 -0600

    chore: init project

---
+++ src/components/TickIcon.tsx
@@ -0,0 +1,3 @@
+export const TickIcon = () => {
+  return <div>TickIcon</div>;
+};
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './Auth';
+export * from './ListHeader';
+export * from './ListItem';
+export * from './Modal';
+export * from './ProgressBar';
+export * from './TickIcon';
