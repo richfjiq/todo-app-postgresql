db8887f6461eadc96b036e3bee62808b745b7392
richfjiq <rfjiq1986@gmail.com>
2023-05-09 07:41:47 -0600

    chore: fetch todos and add styles to ListItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-import { ListHeader } from './components';
+import { useEffect, useState } from 'react';
+
+import { ListHeader, ListItem } from './components';
+
+export interface Task {
+  date: string;
+  id: string;
+  progress: number;
+  title: string;
+  user_email: string;
+}
 
 const App = () => {
+  const userEmail = 'richard@test.com';
+  const [tasks, setTasks] = useState<Task[]>();
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+      setTasks(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log({ tasks });
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+  );
+
   return (
     <div className="app">
       <ListHeader listName={'Holiday tick list'} />
+      {sortedTasks?.map((task) => (
+        <ListItem key={task.id} task={task} />
+      ))}
     </div>
   );
 };
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,3 +1,24 @@
-export const ListItem = () => {
-  return <div>ListItem</div>;
+import { FC } from 'react';
+
+import { Task } from '../App';
+import { ProgressBar, TickIcon } from './';
+
+interface Props {
+  task: Task;
+}
+
+export const ListItem: FC<Props> = ({ task }) => {
+  return (
+    <div className="list_item">
+      <div className="info_container">
+        <TickIcon />
+        <p className="task_title">{task.title}</p>
+        <ProgressBar />
+      </div>
+      <div className="button_container">
+        <button className="edit">EDIT</button>
+        <button className="delete">DELETE</button>
+      </div>
+    </div>
+  );
 };
